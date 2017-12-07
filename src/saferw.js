063691d916c4f426ecdc1a86bc4c6e11e12d7f61
jbastias <jorgebastias@hotmail.com>
2017-12-06 20:40:35 -0600

    checkAndWait before locking a file

--- src/saferw.js
+++ src/saferw.js
@@ -1,8 +1,24 @@
 import lockfile from 'lockfile';
 import fs from 'mz/fs';
 
+export function delay(ms = 100) {
+  return new Promise(resolve => setTimeout(() => resolve(ms), ms));
+}
+
+export function checkAndWait(path, wait = 50, repeat = 3 ) {
+  // console.log(path, wait, repeat);
+  return check(path).then(res => {
+    if (!res) {
+      return res;
+    } else {
+      return delay(wait).then(() => { return repeat && checkAndWait(path, wait, repeat - 1) || res });
+    }
+  });
+}
+
 export function lock(path, options = {}) {
   return new Promise((resolve, reject) => {
+    // console.log('lock options: ', options);
     lockfile.lock(path, options, err => {
       if (err) return reject(err);
       resolve(true);
@@ -29,17 +45,27 @@ export function check(path, options = {}) {
 }
 
 export function safeWrite(path, data, options = { encoding: 'utf8' }) {
+  options.encoding = options.encoding || 'utf8';
   const lockPath = `${path}.lock`;
-  return lock(lockPath)
+    // console.log(path, lockPath, data, options);
+  return checkAndWait(lockPath).then(busy => {
+      // console.log('busy: ', busy);
+      if (busy) throw new Error(`file locked: ${lockPath}`);
+      return lock(lockPath, options);
+    }) 
     .then(() => fs.writeFile(path, data, options))
+    // .then(() => fs.readFile(path).then(data => console.log(`data: ${data}`)))
     .then(() => unlock(lockPath));
 }
 
 export function safeRead(path, options = { encoding: 'utf8' }) {
   const lockPath = `${path}.lock`;
   let data = '';
-  return lock(lockPath)
-    .then(() => fs.readFile(path, options))
+  // return lock(lockPath)
+  return checkAndWait(lockPath).then(busy => {
+    if (busy) throw new Error(`file locked: ${lockPath}`);
+    return lock(lockPath, options);
+  }).then(() => fs.readFile(path, options))
     .then(_data => data = _data)
     .then(() => unlock(lockPath))
     .then(() => data);
--- src/test/saferw.spec.js
+++ src/test/saferw.spec.js
@@ -1,9 +1,57 @@
 import { expect } from 'chai';
 import fs from 'mz/fs';
 import randomstring from 'randomstring';
-import { lock, unlock, check, safeWrite, safeRead } from '../saferw';
+import { delay, checkAndWait, lock, unlock, check, safeWrite, safeRead } from '../saferw';
 
 const tempfile = (prefix = 'temp') => `/tmp/${prefix}-${randomstring.generate(8)}`;
+
+describe('checkAndWait', () => {
+  let filePath;
+
+  beforeEach(() => filePath = tempfile());
+  
+  describe('checkAndWait checks for file lock, waits and repeats', () => {
+    it('should return false after 30 ms', done => {
+      const start = +new Date();
+      setTimeout(() => fs.unlink(filePath), 30);
+      fs.writeFile(filePath, 'STUFF').then(() => checkAndWait(filePath, 10, 10)).then(res => {
+        const end = +new Date();
+        const diff = end - start;
+        // console.log('diff: ', diff, res, filePath);
+        expect(diff).to.be.above(30);
+        expect(res).to.be.false;
+        done();
+      });
+    });
+
+    it('should return true after 100 ms', done => {
+      const start = +new Date();
+      setTimeout(() => fs.unlink(filePath), 150);
+
+      fs.writeFile(filePath, 'STUFF').then(() => checkAndWait(filePath, 10, 10)).then(res => {
+        const end = +new Date();
+        const diff = end - start;
+        // console.log('diff: ', diff, res, filePath);
+        expect(diff).to.be.above(100);
+        expect(res).to.be.true;
+        done();
+      });
+    });
+
+  });  
+});  
+
+describe('delay', () => {
+  const start = +new Date();
+  it('should return false if file does not exist', done => {
+    delay(20).then(delay => { 
+      const end = +new Date();
+      const diff = end - start;
+      expect(diff).to.be.above(delay);
+      done(); 
+    });
+  });  
+});  
 
 describe('lockfile', () => {
 
@@ -15,13 +63,24 @@ describe('lockfile', () => {
         .then(() => done());
     });
 
-    it('should return false if file does not exist', done => {
+    it('should return true if file does exist', done => {
       lock(path)
         .then(() => check(path))
         .then(checkResult => expect(checkResult).to.be.equal(true))
         .then(() => fs.unlink(path))
         .then(() => done());
     });
+
+    it('should fail if lock file exists', done => {
+      lock(path)
+        .then(() => lock(path, { retries: 13, wait: 100 }))
+        .catch(err => { 
+          // console.log(err); 
+          done();
+        });
+    });
+
+
   });
 
   describe('lock', () => {
@@ -76,7 +135,9 @@ describe('lockfile', () => {
   });
 
   describe('safeWrite', () => {
-    const path = tempfile('safe-write');
+    let path;
+    beforeEach(() => path = tempfile('safe-write'));
+
     it("should write file safely", done => {
       safeWrite(path, 'SAFEWRITE')
         .then(() => fs.readFile(path, 'utf8'))
@@ -84,10 +145,35 @@ describe('lockfile', () => {
         .then(() => fs.unlink(path))
         .then(() => done());
     });
+
+    it("should fail lock file already locked", done => {
+      lock(`${path}.lock`)
+        .then(() => safeWrite(path, 'SAFEWRITE', { retries: 10, wait: 1000 }))
+        .catch(err => {
+          // console.log('already locked', err.toString());
+          expect(/file locked/.test(err)).to.be.true;
+          done();
+        });
+    });
+
+    it("should succeed lock file is lock file is removed", done => {
+      const lockPath = `${path}.lock`;
+      setTimeout(() => fs.unlink(lockPath), 50);
+      lock(`${path}.lock`)
+        .then(() => safeWrite(path, 'SAFEWRITE', { retries: 10, wait: 1000 }))
+        .then(() => {
+          return fs.readFile(path, 'utf8').then(data => {
+            expect(data).to.be.equal('SAFEWRITE');
+            return fs.unlink(path).then(() => done());
+          });
+        });
+    });
   });
 
   describe('safeRead', () => {
-    const path = tempfile('safe-read');
+    let path;
+    beforeEach(() => path = tempfile('safe-read'));
+
     it("should read file safely", done => {
       safeWrite(path, 'SAFEREAD')
         .then(() => safeRead(path))
@@ -95,6 +181,28 @@ describe('lockfile', () => {
         .then(() => fs.unlink(path))
         .then(() => done());
     });
+
+    it("should fail lock file already locked", done => {
+      lock(`${path}.lock`)
+        .then(() => safeRead(path))
+        .catch(err => {
+          // console.log('already locked', err.toString());
+          expect(/file locked/.test(err)).to.be.true;
+          done();
+        });
+    });
+
+    it("should succeed lock file is lock file is removed", done => {
+      const lockPath = `${path}.lock`;
+      setTimeout(() => fs.unlink(lockPath), 50);
+      lock(lockPath)
+        .then(() => safeWrite(path, 'SAFEREAD'))
+        .then(() => safeRead(path))
+        .then(data => expect(data).to.be.equal('SAFEREAD'))
+        .then(() => fs.unlink(path))
+        .then(() => done());
+    });
+
   });
 
 });
